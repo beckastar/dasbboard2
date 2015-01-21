31e5cbca44233c93042751b63b76b9b24fe731c3
Becka <rebecca.robbins.et@gmail.com>
2015-01-21 10:30:17 -0800

    blah

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,128 @@
+$(function () {
+  $('#gauge_chart2').highcharts({
+        chart: {
+            type: 'gauge',
+            spacingTop: -100,
+            marginLeft: 50,
+            // align: 'center',
+            verticalAlign: 'top',
+            // spacingBottom: 30,
+            width: 250
+            // plotBackgroundColor: null,
+            // plotBackgroundImage: null,
+            // plotBorderWidth: 0,
+            // plotShadow: false
+        },
+        plotOptions: {
+          center: ['50%','50%'],
+        },
+        title: {
+            text: ''
+        },
+        exporting: { enabled: false },
+        pane: {
+            startAngle: -150,
+            endAngle: 150,
+            background: [{
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#FFF'],
+                        [1, '#333']
+                    ]
+                },
+                borderWidth: 0,
+                outerRadius: '109%'
+            }, {
+                backgroundColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, '#333'],
+                        [1, '#FFF']
+                    ]
+                },
+                borderWidth: 1,
+                outerRadius: '107%'
+            }, {
+                // default background
+            }, {
+                backgroundColor: '#DDD',
+                borderWidth: 0,
+                outerRadius: '105%',
+                innerRadius: '103%'
+            }]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 200,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#666',
+
+            tickPixelInterval: 30,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#666',
+            labels: {
+                step: 2,
+                rotation: 'auto'
+            },
+            title: {
+                text: 'km/h'
+            },
+            plotBands: [{
+                from: 0,
+                to: 120,
+                color: '#55BF3B' // green
+            }, {
+                from: 120,
+                to: 160,
+                color: '#DDDF0D' // yellow
+            }, {
+                from: 160,
+                to: 200,
+                color: '#DF5353' // red
+            }]
+        },
+
+        series: [{
+            name: 'Speed',
+            data: [80],
+            tooltip: {
+                valueSuffix: ' Average Minutes'
+            }
+        }]
+
+    },
+        // Add some life
+        function (chart) {
+            if (!chart.renderer.forExport) {
+                setInterval(function () {
+                    var point = chart.series[0].points[0],
+                        newVal,
+                        inc = Math.round((Math.random() - 0.5) * 20);
+
+                    newVal = point.y + inc;
+                    if (newVal < 0 || newVal > 200) {
+                        newVal = point.y - inc;
+                    }
+
+                    point.update(newVal);
+
+                }, 3000);
+            }
+        });
+});
+
+
+
+
 //chart1
 $(function () {
     $('#chart1').highcharts({
@@ -175,6 +300,7 @@ $(function () {
       $('#line_graph').highcharts({
           chart: {
               type: 'column',
+              height: 270
           },
 
           title: {
